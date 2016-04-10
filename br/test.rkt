4c1d0ea5372ce5a872c0b3fb7538321b6d240d78
Matthew Butterick <mb@mbtype.com>
2016-04-10 16:28:49 -0700

    move files to `beautiful-racket-lib`

--- br/test.rkt
+++
@@ -1,5 +0,0 @@
-#lang racket/base
-(provide message)
-(define message "You installed beautiful-racket correctly.")
-(module+ main
-  (displayln message))
--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 (define collection 'multi)
 
 (define version "0.01")
-(define deps '("base" "sugar" "rackunit-lib" "ragg"))
+(define deps '("base" "sugar" "beautiful-racket-lib" "rackunit-lib" "ragg"))
 (define build-deps '("racket-doc"))
 
 (define test-omit-paths '("br-bf"))
