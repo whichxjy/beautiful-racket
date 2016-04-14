81ab412ca24386d860780f1678055a8fd326b514
Matthew Butterick <mb@mbtype.com>
2016-04-14 13:13:31 -0700

    renames

--- beautiful-racket/br/bf/bf-hash-sexp.rkt
+++
@@ -1,2 +0,0 @@
-#lang s-exp br/bf
-(bf-program (op "+") (op "+") (op "+") (op "+") (op "+") (op "+") (op "+") (loop "[" (op ">") (op "+") (op "+") (op "+") (op "+") (op "+") (op "<") (op "-") "]") (op ">") (op "."))
--- beautiful-racket/br/bf/bf-hash.rkt
+++
@@ -1,2 +0,0 @@
-#lang reader "bf-reader.rkt"
-+++++++[>+++++<-]>.
