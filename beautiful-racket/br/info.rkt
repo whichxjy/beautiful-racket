e79b13153349cdac87a0f5c0f16f994c8652c1ab
Matthew Butterick <mb@mbtype.com>
2016-04-18 23:23:58 -0700

    move info

---
+++ beautiful-racket/br/info.rkt
@@ -0,0 +1,5 @@
+#lang info
+(define collection 'multi)
+
+(define compile-omit-paths '("demo"))
+(define test-omit-paths '("demo"))
--- beautiful-racket/info.rkt
+++ beautiful-racket/info.rkt
@@ -4,6 +4,3 @@
 (define version "0.01")
 (define deps '("base" "sugar" "beautiful-racket-lib" "rackunit-lib" "ragg" "parser-tools-lib"))
 (define build-deps '("racket-doc"))
-
-(define compile-omit-paths '("br/demo"))
-(define test-omit-paths '("br/demo"))
