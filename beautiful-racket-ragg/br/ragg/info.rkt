894c9780d8a182913fc9a9bf1e5b8ec843069112
Matthew Butterick <mb@mbtype.com>
2016-05-04 10:24:40 -0700

    change name of br/ragg doc file

--- beautiful-racket-ragg/br/ragg/info.rkt
+++ beautiful-racket-ragg/br/ragg/info.rkt
@@ -5,7 +5,7 @@
 (define required-core-version "5.3.1")
 (define version "1.0")
 (define repositories '("4.x"))
-(define scribblings '(("ragg.scrbl")))
+(define scribblings '(("br-ragg.scrbl")))
 (define blurb '("ragg: a Racket AST Generator Generator.  A design goal is to be easy for beginners to use. Given a grammar in EBNF, ragg produces a parser that generates Racket's native syntax objects with full source location."))
 (define release-notes '((p "First release.")))
 (define deps (list))
