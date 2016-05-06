8dea96894b6965189bfb873fbf24751c2e440935
Matthew Butterick <mb@mbtype.com>
2016-05-06 10:34:43 -0700

    add dependency

--- brag/info.rkt
+++ brag/info.rkt
@@ -1,6 +1,6 @@
 #lang setup/infotab
 
-(define deps '("base" "parser-tools-lib" "rackunit-lib" "python-tokenizer"))
+(define deps '("base" "beautiful-racket-lib" "parser-tools-lib" "rackunit-lib" "python-tokenizer"))
 (define build-deps '("at-exp-lib" "parser-tools-doc" "racket-doc"
                      "scribble-lib"))
 (define collection 'multi)
