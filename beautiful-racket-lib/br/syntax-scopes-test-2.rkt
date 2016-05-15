5ea796cbcd4f7c9576cf5a23d621dd15c0617e9a
Matthew Butterick <mb@mbtype.com>
2016-05-15 09:56:30 -0700

    one more

---
+++ beautiful-racket-lib/br/syntax-scopes-test-2.rkt
@@ -0,0 +1,21 @@
+#lang br
+(require (for-syntax br/syntax))
+
+(begin-for-syntax
+  (define-scope blue))
+
+(define #'(define-blue _id _expr)
+  (with-syntax ([_id (blue-binding-form #'_id)])
+    #'(define _id _expr)))
+
+(define #'(blue _id)
+  (with-syntax ([_id (blue #'_id)])
+    #'_id))
+
+(define-syntax x:blue (make-rename-transformer (with-syntax ([x (blue #'x)])
+                                                 #'x)))
+
+(define-blue x (+ 42 42))
+(define y 50)
+
+(+ (blue x) y)
