90b8680bd7d5a3f4038faeb16897c9bc05dd5bcc
Matthew Butterick <mb@mbtype.com>
2016-07-10 15:22:43 -0700

    compress

--- beautiful-racket/br/demo/stacker3.rkt
+++ beautiful-racket/br/demo/stacker3.rkt
@@ -1,21 +1,22 @@
 #lang br/quicklang
-(provide read-syntax (rename-out [stacker-module-begin #%module-begin]) + *)
+(provide read-syntax
+         (rename-out [stacker-module-begin #%module-begin])
+         + *)
 
 (define (read-syntax path port)
-  (strip-context
-   #`(module stacker3-mod br/demo/stacker3
-       #,@(port->list read port))))
+  (datum->syntax #f
+                 `(module stacker3-mod br/demo/stacker3
+                      ,@(port->list read port))))
 
 (define-macro (stacker-module-begin ARG ...)
   #'(#%module-begin
      (display (first
                (foldl
-                (λ(x xs)
+                (λ (x xs)
                   (if (number? x)
                       (cons x xs)
                       (cons (x (car xs) (cadr xs)) (cddr xs))))
                 null (list ARG ...))))))
-
 
 (module+ test 
   (require rackunit)
