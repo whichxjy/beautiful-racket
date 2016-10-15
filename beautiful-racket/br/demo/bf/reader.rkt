9014efcb5366abff1631158a7c1a01231c2d195d
Matthew Butterick <mb@mbtype.com>
2016-10-15 00:06:08 -0700

    renames

--- beautiful-racket/br/demo/bf/reader.rkt
+++ beautiful-racket/br/demo/bf/reader.rkt
@@ -1,14 +1,14 @@
 #lang br/quicklang
-(require "bf-parser.rkt")
+(require "parser.rkt")
 
 (define (read-syntax path port)
   (define parse-tree (parse path (tokenize port)))
-  (define module-datum `(module bf-mod br/demo/bf/bf-expander
+  (define module-datum `(module bf-mod br/demo/bf/expander
                           ,parse-tree))
   (datum->syntax #f module-datum))
 (provide read-syntax)
 
-(require parser-tools/lex brag/support)
+(require parser-tools/lex)
 (define (tokenize port)
   (define (next-token)
     (define our-lexer
