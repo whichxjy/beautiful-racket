1c3f8781f78675261439bb2130b32b420c00efb7
Matthew Butterick <mb@mbtype.com>
2016-11-08 07:27:46 -0800

    improve jsonic

--- beautiful-racket/br/demo/jsonic/reader.rkt
+++ beautiful-racket/br/demo/jsonic/reader.rkt
@@ -2,7 +2,7 @@
 (require "tokenizer.rkt" "parser.rkt")
 (define (read-syntax path port)
   (define parse-tree (parse path (tokenize port)))
-  (define module-datum `(module jsonic-module "expander.rkt"
+  (define module-datum `(module jsonic-module br/demo/jsonic/expander
                           ,parse-tree))
   (datum->syntax #f module-datum))
 (provide read-syntax)
--- beautiful-racket/br/demo/jsonic/tokenizer.rkt
+++ beautiful-racket/br/demo/jsonic/tokenizer.rkt
@@ -1,5 +1,5 @@
 #lang br/quicklang
-(require parser-tools/lex parser-tools/lex-sre brag/support)
+(require brag/lexer-support)
 (define (tokenize port)
   (define (next-token)
     (define our-lexer
@@ -7,9 +7,9 @@
        [(eof) eof]
        ;; (char-complement "\n") means any char but "\n"
        ;; (complement "\n") means any whole string except "\n"
-       [(seq "//" (* (char-complement "\n"))) (next-token)]
-       ["@$" (token 'OPEN lexeme)]
-       ["$@" (token 'CLOSE lexeme)]
+       [(:seq "//" (:* (char-complement "\n"))) (next-token)]
+       ["@$" (token 'OPEN)]
+       ["$@" (token 'CLOSE)]
        [any-char (token 'CHAR lexeme)]))
     (our-lexer port))  
   next-token)
