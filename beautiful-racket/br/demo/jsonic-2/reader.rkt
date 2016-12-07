b6a45815abcde54d6821e147d263111dd20135e1
Matthew Butterick <mb@mbtype.com>
2016-12-07 11:06:42 -0800

    updates

--- beautiful-racket/br/demo/jsonic-2/reader.rkt
+++ beautiful-racket/br/demo/jsonic-2/reader.rkt
@@ -1,12 +1,10 @@
 #lang br/quicklang
 (require "tokenizer.rkt" "parser.rkt")
-(define (syntax-no-bindings? stx)
-  (and (syntax? stx)
-       (equal? stx (strip-bindings stx))))
+
 (define/contract (read-syntax path port)
   (path? input-port? . -> . syntax?)
   (define parse-tree (parse path (tokenize port)))
-  (define module-datum `(module jsonic-module br/demo/jsonic/expander
+  (define module-datum `(module jsonic-module jsonic/expander
                           ,parse-tree))
   (datum->syntax #f module-datum))
 (provide read-syntax)
--- beautiful-racket/br/demo/jsonic-2/tokenizer.rkt
+++ beautiful-racket/br/demo/jsonic-2/tokenizer.rkt
@@ -1,8 +1,17 @@
 #lang br/quicklang
 (require brag/support)
 
+(module+ test
+  (require rackunit))
+
 (define (token? x)
   (or (eof-object? x) (string? x) (token-struct? x)))
+
+(module+ test
+  (check-true (token? eof))
+  (check-true (token? "a string"))
+  (check-true (token? (token 'A-TOKEN-STRUCT "hi")))
+  (check-false (token? 42)))
 
 (define/contract (tokenize port)
   (input-port? . -> . (-> token?))
@@ -18,3 +27,13 @@
     (our-lexer port))  
   next-token)
 (provide tokenize)
+
+(module+ test
+  (check-equal? (apply-tokenizer tokenize "// comment\n") empty)
+  (check-equal?
+   (apply-tokenizer tokenize "@$ (+ 6 7) $@")
+   (list (token-struct 'SEXP-TOK " (+ 6 7) " #f #f #f #f #f)))
+  (check-equal?
+   (apply-tokenizer tokenize "hi")
+   (list (token-struct 'CHAR-TOK "h" #f #f #f #f #f)
+         (token-struct 'CHAR-TOK "i" #f #f #f #f #f))))
