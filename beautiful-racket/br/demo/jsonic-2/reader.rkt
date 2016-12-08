58ae9d5c08a96d926ca754d6dfa1d4c7f0b0ae90
Matthew Butterick <mb@mbtype.com>
2016-12-08 15:58:38 -0800

    repair read-syntax

--- beautiful-racket/br/demo/jsonic-2/reader.rkt
+++ beautiful-racket/br/demo/jsonic-2/reader.rkt
@@ -2,9 +2,9 @@
 (require "tokenizer.rkt" "parser.rkt")
 
 (define/contract (read-syntax path port)
-  (path? input-port? . -> . syntax?)
+  (any/c input-port? . -> . syntax?)
   (define parse-tree (parse path (tokenize port)))
-  (define module-datum `(module jsonic-module jsonic/expander
+  (define module-datum `(module jsonic-module br/demo/jsonic-2/expander
                           ,parse-tree))
   (datum->syntax #f module-datum))
 (provide read-syntax)
