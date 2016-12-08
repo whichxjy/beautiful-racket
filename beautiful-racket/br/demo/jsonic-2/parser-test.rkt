747e59ebc5da767861a23b75d047d9827941d541
Matthew Butterick <mb@mbtype.com>
2016-12-08 09:09:07 -0800

    parser test

--- beautiful-racket/br/demo/jsonic-2/parser-test.rkt
+++ beautiful-racket/br/demo/jsonic-2/parser-test.rkt
@@ -1,4 +1,24 @@
-#lang brag
-jsonic-program: (s-exp | json-char)*
-s-exp: SEXP-TOK
-json-char: CHAR-TOK
+#lang br
+(require br/demo/jsonic-2/parser
+         br/demo/jsonic-2/tokenizer
+         brag/support
+         rackunit)
+
+(check-equal?
+ (parse-tree (apply-tokenizer tokenize "// line commment\n"))
+ '(jsonic-program))
+(check-equal?
+ (parse-tree (apply-tokenizer tokenize "@$ 42 $@"))
+ '(jsonic-program (s-exp " 42 ")))
+(check-equal?
+ (parse-tree (apply-tokenizer tokenize "hi"))
+ '(jsonic-program
+   (json-char "h")
+   (json-char "i")))
+(check-equal?
+ (parse-tree (apply-tokenizer tokenize "hi\n// comment\n@$ 42 $@"))
+ '(jsonic-program
+   (json-char "h")
+   (json-char "i")
+   (json-char "\n")
+   (s-exp " 42 ")))
