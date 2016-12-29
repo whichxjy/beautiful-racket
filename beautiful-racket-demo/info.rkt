2963ed68603d639d152b45343b463d6c549185aa
Matthew Butterick <mb@mbtype.com>
2016-12-29 05:58:24 -0800

    update info

--- beautiful-racket-demo/info.rkt
+++ beautiful-racket-demo/info.rkt
@@ -4,3 +4,5 @@
 (define version "0.01")
 (define deps '("base" "sugar" "beautiful-racket-lib" "rackunit-lib" "brag" "parser-tools-lib"))
 (define build-deps '("racket-doc"))
+(define compile-omit-paths 'all)
+(define test-omit-paths 'all)
