c4f90daf74c271d2864196e09105626f5b66862e
Matthew Butterick <mb@mbtype.com>
2017-01-29 12:14:44 -0800

    spaces

--- beautiful-racket-demo/basic-demo/parser.rkt
+++ beautiful-racket-demo/basic-demo/parser.rkt
@@ -3,9 +3,9 @@ b-program : b-line*
 b-line : @b-line-number b-statement (/":" b-statement)*
 b-line-number : INTEGER
 @b-statement : b-rem
-            | b-print
-            | b-goto
-            | b-end
+             | b-print
+             | b-goto
+             | b-end
 b-rem : REM
 b-print : /"print" (STRING | b-expr)*
 b-goto : /"goto" b-expr
