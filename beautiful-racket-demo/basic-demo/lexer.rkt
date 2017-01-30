85ecc17a9d27b00e9e770fca882c8990f39f5a2a
Matthew Butterick <mb@mbtype.com>
2017-01-30 08:47:45 -0800

    more

--- beautiful-racket-demo/basic-demo/lexer.rkt
+++ beautiful-racket-demo/basic-demo/lexer.rkt
@@ -1,15 +1,16 @@
 #lang br
 (require brag/support)
 
+(define-lex-abbrev digits (:+ (char-set "0123456789")))
+
 (define basic-lexer
   (lexer-srcloc
    [(eof) (return-without-srcloc eof)]
    [whitespace (token lexeme #:skip? #t)]
-   [(from/to "rem" #\newline) (token 'REM lexeme)]
+   [(from/to "rem" "\n") (token 'REM lexeme)]
    [(:or "print" "goto" "end" "+" ":") lexeme]
-   [(:+ numeric) (token 'INTEGER (string->number lexeme))]
-   [(:or (:seq (:+ numeric) ".")
-         (:seq (:* numeric) "." (:+ numeric)))
+   [digits (token 'INTEGER (string->number lexeme))]
+   [(:or (:seq digits ".") (:seq (:? digits) "." digits))
     (token 'DECIMAL (string->number lexeme))]
    [(from/to "\"" "\"")
     (token 'STRING (trim-ends "\"" lexeme "\""))]))
