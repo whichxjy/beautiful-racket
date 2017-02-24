e571d9b37289f713995e19b25354d96702e85ee3
Matthew Butterick <mb@mbtype.com>
2017-02-24 08:34:52 -0800

    support shell args

--- beautiful-racket-demo/basic-demo-3/expander.rkt
+++ beautiful-racket-demo/basic-demo-3/expander.rkt
@@ -8,7 +8,10 @@
       ([((b-line NUM STMT ...) ...) #'(LINE ...)]
        [(LINE-FUNC ...) (prefix-id "line-" #'(NUM ...))]
        [(VAR-ID ...) (find-property 'b-id #'(LINE ...))]
-       [(REQ-SPEC ...) (find-property 'b-require-spec #'(LINE ...))])
+       [(REQ-SPEC ...) (find-property 'b-require-spec #'(LINE ...))]
+       [((SHELL-ID SHELL-VAL) ...)
+        (for/list ([(arg idx) (in-indexed (current-command-line-arguments))])
+          (list (format-id caller-stx "arg~a" idx) arg))]) ; explain why (format-datum 'arg~a idx) won't work
     #'(#%module-begin
        (module configure-runtime br
          (require "runtime.rkt")
@@ -17,12 +20,13 @@
        (require REQ-SPEC) ...
        (define VAR-ID 0) ...
        (provide VAR-ID ...)
+       (set! SHELL-ID SHELL-VAL) ...
        LINE ...
        (define line-table
          (apply hasheqv (append (list NUM LINE-FUNC) ...)))
        (parameterize ([current-output-port
-                             (or (current-basic-port) (open-output-nowhere))])
-               (void (run line-table))))))
+                       (or (current-basic-port) (open-output-nowhere))])
+         (void (run line-table))))))
 
 (begin-for-syntax
   (require racket/list)
---
+++ beautiful-racket-demo/basic-demo-3/sample-shell.rkt
@@ -0,0 +1,2 @@
+#lang basic-demo-3
+10 print "got shell arg" : print arg0 : print arg1
