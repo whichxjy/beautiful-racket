2e69bbda33da80be0f2bfd13603c1a3b64cd12a1
Matthew Butterick <mb@mbtype.com>
2017-02-28 17:06:55 -0800

    fix sample

--- beautiful-racket-demo/basic-demo-3/sample-gosub.rkt
+++ beautiful-racket-demo/basic-demo-3/sample-gosub.rkt
@@ -1,10 +1,10 @@
 #lang basic-demo-3
 10 gosub 41
-20 print "world"
+20 print "second"
 30 gosub 100
-31 print "hi"
+31 print "fourth"
 35 end
 40 return
-41 print "hello" : return
+41 print "first" : return
 100 print "third"
 110 goto 40
