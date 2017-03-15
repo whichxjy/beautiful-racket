bb4dce853ca8f435bf6b91798caa6d0131614d1b
Matthew Butterick <mb@mbtype.com>
2017-03-15 10:40:16 -0700

    improve test message

--- beautiful-racket-lib/br/test.rkt
+++ beautiful-racket-lib/br/test.rkt
@@ -1,5 +1,8 @@
 #lang racket/base
+(require setup/getinfo racket/runtime-path)
 (provide message)
-(define message "You installed beautiful-racket correctly.")
+(define-runtime-path br-dir "../../beautiful-racket")
+(define gi (get-info/full br-dir))
+(define message (format "You installed beautiful-racket v~a correctly." (gi 'version)))
 (module+ main
   (displayln message))
