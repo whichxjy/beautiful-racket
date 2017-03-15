2f1a2b4520c64ffbf1df027863bf52f8d25cd10e
Matthew Butterick <mb@mbtype.com>
2017-03-15 10:57:20 -0700

    defense

--- beautiful-racket-lib/br/test.rkt
+++ beautiful-racket-lib/br/test.rkt
@@ -3,6 +3,10 @@
 (provide message)
 (define-runtime-path br-dir "../../beautiful-racket")
 (define gi (get-info/full br-dir))
-(define message (format "You installed beautiful-racket v~a correctly." (gi 'version)))
+(define message
+  (format "You installed beautiful-racket~acorrectly."
+          (if gi
+              (format " v~a " (gi 'version))
+              " ")))
 (module+ main
   (displayln message))
