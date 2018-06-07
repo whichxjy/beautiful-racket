ece33a624e773bacfd29d1ff161fd25cdfd44684
Matthew Butterick <mb@mbtype.com>
2018-06-07 12:21:30 -0700

    put `br/module` in #lang br

--- beautiful-racket-lib/br/main.rkt
+++ beautiful-racket-lib/br/main.rkt
@@ -1,15 +1,13 @@
 #lang racket/base
 (require racket/provide racket/list racket/string racket/format racket/match racket/port racket/function racket/provide
-         br/define br/syntax br/datum br/debug br/cond br/list br/reader-utils racket/class racket/vector
+         br/define br/syntax br/datum br/debug br/cond br/list br/reader-utils br/module racket/class racket/vector
          (for-syntax racket/base racket/syntax br/syntax br/debug br/define br/datum))
 (provide (all-from-out racket/base)
          (all-from-out racket/list racket/string racket/format racket/match racket/port racket/function racket/provide
-                       br/syntax br/datum br/debug br/cond br/list br/reader-utils racket/class racket/vector br/define)
+                       br/syntax br/datum br/debug br/cond br/list br/reader-utils br/module racket/class racket/vector br/define)
          (for-syntax (all-from-out racket/base racket/syntax br/syntax br/debug br/datum))
          (for-syntax caller-stx with-shared-id)) ; from br/define
          
-;; todo: activate at-exp reader by default
-
 (provide evaluate)
 (define-macro (evaluate DATUM)
   #'(begin
