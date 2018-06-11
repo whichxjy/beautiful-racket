8a11393b033d149028a4702ce6e440d80c947531
Matthew Butterick <mb@mbtype.com>
2018-06-11 10:23:20 -0700

    update deps

--- beautiful-racket-lib/info.rkt
+++ beautiful-racket-lib/info.rkt
@@ -2,7 +2,8 @@
 (define collection 'multi)
 
 (define version "1.1")
-(define deps '("base"
+(define deps '("at-exp-lib"
+               "base"
                "sugar"
                "gui-lib"
                "draw-lib"))
