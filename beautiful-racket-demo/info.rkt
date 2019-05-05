1116d43e588594e0380c30d9489364334e3f5fef
Matthew Butterick <mb@mbtype.com>
2019-05-05 09:21:32 -0700

    note

--- beautiful-racket-demo/info.rkt
+++ beautiful-racket-demo/info.rkt
@@ -7,6 +7,8 @@
 
 (define version "1.5")
 
+;; base v6.7 dependency needs to be called 6.6.0.900 
+;; due to strange little bug in `raco pkg install`
 (define deps '(["base" #:version "6.6.0.900"]
                "sugar"
                "beautiful-racket-lib"
