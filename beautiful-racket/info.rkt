eec4502c4c63f10bdeb7911e43496034ec3d2b35
Matthew Butterick <mb@mbtype.com>
2019-05-05 07:06:02 -0700

    bump to 1.5; min version 6.7

--- beautiful-racket/info.rkt
+++ beautiful-racket/info.rkt
@@ -1,9 +1,9 @@
 #lang info
 (define collection 'multi)
 
-(define version "1.4")
+(define version "1.5")
 
-(define deps '(["base" #:version "6.3"]
+(define deps '(["base" #:version "6.7"]
                "beautiful-racket-lib"
                "beautiful-racket-demo"))
 
