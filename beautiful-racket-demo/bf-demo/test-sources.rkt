2a4ec25b19c0fa12ad12a54aa08ca6240c040a81
whichxjy <whichxjy@gmail.com>
2019-05-25 19:41:24 +0800

    Add unit test for cow.rkt

--- beautiful-racket-demo/bf-demo/test-sources.rkt
+++ beautiful-racket-demo/bf-demo/test-sources.rkt
@@ -1,6 +1,8 @@
 #lang br
 (require br/test rackunit)
- 
-(check-equal? (run-source "atsign.rkt") "@")
-(check-equal? (run-source "atsign-sexp.rkt") "@")
-(check-equal? (run-source "hello.rkt") "Hello, World!")
+
+(check-equal? (run-source "atsign.rkt") "@")
+(check-equal? (run-source "atsign-sexp.rkt") "@")
+(check-equal? (run-source "hello.rkt") "Hello, World!")
+(check-equal? (run-source "cow.rkt")
+              "^__^\n(oo)\\_______\n(__)\\       )\\/\\\n    ||----w |\n    ||     ||\n")
